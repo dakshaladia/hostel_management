99da92bcb7140fd9b0d42e34e64e74045022232a
Daksha <ddladia58@hotmail.com>
2017-11-08 15:36:16 +0530

    Add existing file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
-export default class App extends React.Component {
+/*export default class App extends React.Component {
   render() {
     return (
      <div style={{textAlign: 'center'}}>
         <h1>Hello World From react component</h1>
       </div>);
   }
+}*/
+
+ 
+export default class App extends React.Component {
+  render() {
+    return (
+    <div id="parent">
+      <div style={{textAlign:'center'}}>
+    <h1>LETS SIGN UP !</h1>
+         </div>  
+        <div style={{textAlign:'left'}}>
+            <h4>Login:</h4>
+            <input type="text" value= "" placeholder="Enter e-mail"/>
+               
+          
+         <h4>Password:</h4>
+       
+          <input type="text" value="" placeholder="Enter password"/>
+          <div>
+          <input type="button" value="LogIn"/>
+         </div>
+         </div>
+         </div>);
+    }
 }
+       
