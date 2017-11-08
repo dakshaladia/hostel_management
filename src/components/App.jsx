1a9ae6cfe0b3c9275b456b05c8e0725cf6bcf07c
Daksha <ddladia58@hotmail.com>
2017-11-08 22:03:32 +0530

    Committing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,25 +11,86 @@ import React from 'react';
 
  
 export default class App extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.state = {cool: ''};
+     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangee = this.handleChangee.bind(this);
+    this.handleSubmitt = this.handleSubmitt.bind(this);
+  }
+   handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Username:' + this.state.value);
+ 
+    event.preventDefault();
+  }
+  handleChangee(eventt) {
+    this.setState({value: eventt.target.value});
+  }
+
+  handleSubmitt(eventt) {
+    
+    alert('Password:' + this.state.value);
+    eventt.preventDefault();
+  }
   render() {
     return (
     <div id="parent">
       <div style={{textAlign:'center'}}>
     <h1>LETS SIGN UP !</h1>
-         </div>  
-        <div style={{textAlign:'left'}}>
-            <h4>Login:</h4>
-            <input type="text" value= "" placeholder="Enter e-mail"/>
-               
-          
-         <h4>Password:</h4>
-       
-          <input type="text" value="" placeholder="Enter password"/>
-          <div>
-          <input type="button" value="LogIn"/>
-         </div>
-         </div>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <br>
+          </br>
+          <form onSubmit={this.handleSubmitt}>
+          <form onSubmit={this.handleSubmit}>
+         
+          <label>
+          Username:
+            <input type="text" value= {this.state.value} onChange={this.handleChange} placeholder="Enter e-mail"/>
+               </label>
+               <br>
+               </br>
+               <label>
+               Password:
+               <input type="text" value={this.state.value} onChange={this.handleChangee} placeholder="Enter password"/>
+               </label>
+               <br>
+               </br>
+               <br>
+               </br>
+          <input type="submit" value="SignIn" />
+      </form>
+    </form>
+    </div> 
          </div>);
     }
 }
        
+  /*  <div style={{textAlign:'left'}}>
+            <h4>Username:</h4>
+            <input type="text" value= {this.state.value} onChange={this.handleChange} placeholder="Enter e-mail"/>
+               
+          
+         <h4>Password:</h4>
+       
+          <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Enter password"/>
+          <div>
+          <input type="button" value="LogIn"/>
+         </div>
+         </div>*/
