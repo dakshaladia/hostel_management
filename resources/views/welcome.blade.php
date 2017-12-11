d66e860ecb1ac5155ce86e009a43b2fcc2178682
Daksha <ddladia58@hotmail.com>
2017-12-11 17:07:24 +0530

    homeepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+/*<!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
@@ -86,4 +86,65 @@
             </div>
         </div>
     </body>
+</html>*/
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<html>
+<head>
+<style>
+body{
+background-color:lightcyan;
+ text-align:center;
+ width:50%
+padding:20px;
+ margin:50px;
+ font-size:30px;
+ color:black;
+height:100%;    
+font-size:30px;
+padding:50px;
+}
+.nav {
+  overflow: hidden;
+  background-color: #333;
+}
+.nav a {
+ float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.nav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+.nav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+h1{
+    text-align: center;
+    
+}
+u{
+        color: green;
+    }
+</style>
+
+</head>
+<body>
+<div class="nav">
+  <a class="active" href="#feesstruct">FEES STRUCTURE</a>
+  <a href="">FACILITIES</a>
+  <a href="">ABOUT US</a>
+  
+</div>
+<img src="Hostel_Dormitory.jpg" alt="Students in hostel" style="width:100%;height:200px;">
+<h1><u>TESTIMONIALS</u></h1>
+<p>"What a lovely hostel! Very clean,nice breakfast! The ladies(Tequita and Hannah-sorry for spelling) who worked there were so lovely and told us all the info we needed! Thank You!"</p>
+<p>Vinit Payal(Stay Period 2011-2015)</p>
+</body>
 </html>
